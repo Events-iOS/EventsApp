e25c44b9bace70326f9a443e61a19142cd8fa8ab
Rajit Dang <r3dang@ucsd.edu>
2017-04-17 21:38:40 -0700

    Changed UI

---
+++ Events/EventCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Events
+//
+//  Created by Rajit Dang on 4/17/17.
+//  Copyright © 2017 Events-iOS. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
